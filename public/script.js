c2fa24c833e4dd0ecce091a814860d9239c8340e
Thiago3011 <thiago.silva1001@etec.sp.gov.br>
2022-02-07 19:41:21 -0300

    adicionando os campos de dados

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const buttonNext = document.querySelector('[data-btn-next]')
 const buttonBack = document.querySelector('[data-btn-back]')
 const title = document.getElementsByClassName('main-card-text-title')
 const p = document.getElementsByClassName('main-card-text-p')
+const fields = document.getElementsByClassName('fields-group')
 
 
 var counter = 0
@@ -12,26 +13,30 @@ buttonBack.addEventListener('click', () => {
 
     title[counter].style.display = 'none'
     p[counter].style.display = 'none'
-
+    fields[counter].style.display = 'none'
+    
     if (counter > -1) {
         counter--
     }
-
+    
+    
     counterCheck()
-
+    
 })
 
 buttonNext.addEventListener('click', () => {
-
+    
     title[counter].style.display = 'none'
     p[counter].style.display = 'none'
-
+    fields[counter].style.display = 'none'
+    
+    
     if (counter < 3) {
         counter++
     }
-
+    
     counterCheck()
-
+    
 })
 
 function counterCheck() {
@@ -40,6 +45,7 @@ function counterCheck() {
         case 0:
             title[counter].style.display = 'block'
             p[counter].style.display = 'block'
+            fields[counter].style.display = 'grid'
             buttonBack.style.display= 'none'
             break
 
@@ -47,15 +53,20 @@ function counterCheck() {
             title[counter].style.display = 'block'
             p[counter].style.display = 'block'
             buttonBack.style.display= 'inline-block'
+            fields[counter].style.display = 'grid'
+
             break
 
         case 2:
             title[counter].style.display = 'block'
             p[counter].style.display = 'block'
+            fields[counter].style.display = 'grid'
+
             break
 
         case 3:
             title[counter].style.display = 'block'
+            fields[counter].style.display = 'grid'
             p[counter].style.display = 'block'
             break
         default:
